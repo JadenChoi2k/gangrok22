69e780c4d8aeafca6b668fdadcc513a7f24e6610
JadenChoi2k <jkya02@gmail.com>
2024-07-07 09:51:37 +0900

    implment ingredients scoring

---
+++ Assets/Scripts/Refrige/FoodItemStatusBar.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodItemStatusBar : MonoBehaviour
+{
+    private Canvas canvas;
+
+    void Start()
+    {
+        canvas = GetComponentInChildren<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
